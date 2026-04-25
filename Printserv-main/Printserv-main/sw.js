376ce9d442789b9b2e5cdd55178142a671c8bb7e
thartharthone-svg <thartharthone@gmail.com>
2026-04-26 00:59:44 +0630

    Add files via upload

---
+++ Printserv-main/Printserv-main/sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'printserv-v1';
+const ASSETS = ['/'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(keys.filter(function(k){ return k!==CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
